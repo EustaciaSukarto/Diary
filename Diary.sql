8499e3894403f154d430d0cc6b2ec66a3278a5c9
EustaciaSukarto <66350050+EustaciaSukarto@users.noreply.github.com>
2021-05-29 01:16:49 +0700

    Add files via upload

---
+++ Diary.sql
@@ -0,0 +1,16 @@
+CREATE TABLE user (
+    ID int NOT NULL AUTO_INCREMENT,
+    Fullname varchar(45),
+    Birthday date,
+    Email varchar(45) NOT NULL UNIQUE,
+    Username varchar(45) NOT NULL UNIQUE,
+    Password varchar(45) NOT NULL,
+    PRIMARY KEY (ID)
+);
+CREATE TABLE entry (
+    ID int NOT NULL AUTO_INCREMENT,
+    UserID int NOT NULL,
+    Content longtext,
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES user(ID)
+);
